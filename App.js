0ed6e247ca7d3183b4c1e66927573eb4c70b7d5e
Anji <anji.pune@gmail.com>
2023-10-28 08:24:01 +0530

    parcel config added

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
 //React App
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
+
+
+const heading = React.createElement("h2", {}, "React App");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
